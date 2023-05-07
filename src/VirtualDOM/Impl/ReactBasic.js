44e4d6bd35706e1c169d47e3fffd794364666fc9
Michael Bock <no-day@posteo.net>
2023-05-07 06:54:24 +0200

    Remove key warning

--- src/VirtualDOM/Impl/ReactBasic.js
+++ src/VirtualDOM/Impl/ReactBasic.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 export const createElement = (name) => (props) => (kids) =>
-  React.createElement(name, props, kids);
+  React.createElement(name, props, ...kids);
 
 export const createVoidElement = (name) => (props) =>
   React.createElement(name, props);
